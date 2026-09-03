72203fded5d315d012b835219e39758c0336edf0
razi8 <65502406+razi8@users.noreply.github.com>
2026-09-03 14:39:39 +0700

    fix(output): omit redacted value from JSON results

--- pkg/output/json.go
+++ pkg/output/json.go
@@ -49,10 +49,7 @@ func (p *JSONPrinter) Print(_ context.Context, r *detectors.ResultWithMetadata) 
 		Raw string
 		// RawV2 contains the raw secret identifier that is a combination of both the ID and the secret.
 		// This is used for secrets that are multi part and could have the same ID. Ex: AWS credentials
-		RawV2 string
-		// Redacted contains the redacted version of the raw secret identification data for display purposes.
-		// A secret ID should be used if available.
-		Redacted       string
+		RawV2          string
 		ExtraData      map[string]string
 		StructuredData *detectorspb.StructuredData
 		SecretParts    map[string]string
@@ -70,7 +67,6 @@ func (p *JSONPrinter) Print(_ context.Context, r *detectors.ResultWithMetadata) 
 		VerificationFromCache: r.VerificationFromCache,
 		Raw:                   string(r.Raw),
 		RawV2:                 string(r.RawV2),
-		Redacted:              r.Redacted,
 		ExtraData:             r.ExtraData,
 		StructuredData:        r.StructuredData,
 		SecretParts:           r.SecretParts,
